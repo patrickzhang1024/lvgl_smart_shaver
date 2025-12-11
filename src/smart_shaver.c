49fd93eb5c9c5fe806562c57c2ebbf27316ed43f
patrickz <patrickzhang.engineer@gmail.com>
2025-12-11 21:48:39 +1100

    LVGL-SS: Add four swipe windows

--- src/smart_shaver.c
+++ src/smart_shaver.c
@@ -1,6 +1,6 @@
 #include "smart_shaver.h"
 
-static lv_obj_t * create_window(lv_obj_t *parent, const char *title);
+static lv_obj_t * create_window(lv_obj_t *parent, const char *title, lv_color_t bg_color);
 
 void smart_shaver_ui_init(void)
 {
@@ -16,20 +16,22 @@ void smart_shaver_ui_init(void)
   lv_obj_set_scroll_dir(pager, LV_DIR_HOR);
   lv_obj_set_scroll_snap_x(pager, LV_SCROLL_SNAP_CENTER);
   lv_obj_clear_flag(pager, LV_OBJ_FLAG_SCROLL_ELASTIC);
+  lv_obj_add_flag(pager, LV_OBJ_FLAG_SCROLL_ONE);
+  lv_obj_set_style_pad_all(pager, 0, 0);
 
   /* Create four blank windows */
-  create_window(pager, "Window 1");
-  create_window(pager, "Window 2");
-  create_window(pager, "Window 3");
-  create_window(pager, "Window 4");
+  create_window(pager, "Window 1", lv_color_hex(0xFFFFFF));
+  create_window(pager, "Window 2", lv_color_hex(0x0066FF));
+  create_window(pager, "Window 3", lv_color_hex(0xFFCC00));
+  create_window(pager, "Window 4", lv_color_hex(0xFF3333));
 }
 
-static lv_obj_t * create_window(lv_obj_t *parent, const char *title)
+static lv_obj_t * create_window(lv_obj_t *parent, const char *title, lv_color_t bg_color)
 {
   lv_obj_t *win = lv_obj_create(parent);
   lv_obj_remove_style_all(win);
   lv_obj_set_size(win, SMART_SHAVER_SCREEN_WIDTH, SMART_SHAVER_SCREEN_HEIGHT);
-  lv_obj_set_style_bg_color(win, lv_color_hex(0x202020), 0);
+  lv_obj_set_style_bg_color(win, bg_color, 0);
   lv_obj_set_style_bg_opa(win, LV_OPA_100, 0);
 
   /* Simple title to differentiate the empty windows */
